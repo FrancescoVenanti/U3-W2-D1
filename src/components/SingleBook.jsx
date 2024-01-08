0b47c0553364bf6275d13d56e600c33eb5c86a75
FrancescoVenanti <francesco.venanti@gmail.com>
2024-01-08 16:53:37 +0100

    sdfsf

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -7,22 +7,26 @@ class SingleBook extends Component {
 		selected: false,
 	};
 
+	handleClick = () => {
+		const { selected } = this.state;
+		this.setState({ selected: !selected }, () => {
+			if (this.state.selected) {
+				this.props.setAsinState(this.props.book.asin);
+			}
+		});
+	};
+
 	render() {
 		return (
 			<>
-				<Card
-					onClick={() => this.setState({ selected: !this.state.selected })}
-					style={{ border: this.state.selected ? "3px solid red" : "none" }}
-				>
+				<Card onClick={this.handleClick} style={{ border: this.state.selected ? "3px solid red" : "none" }}>
 					<Card.Img variant="top" src={this.props.book.img} />
 					<Card.Body>
 						<Card.Title style={{ color: "black" }}>{this.props.book.title}</Card.Title>
 					</Card.Body>
 				</Card>
-				{this.state.selected && this.props.setAsinState(this.props.book.asin)}
 			</>
 		);
 	}
 }
-
 export default SingleBook;
